0d76fc6a2d2ceca1543132d86f9884d5e3e65c6a
marianoarias1 <marianoarias123@hotmail.com>
2024-01-18 23:37:49 -0300

    entrega aplicación con Expo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Main} from "./src/components/Main.jsx"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Main name={'Porsche 911 992 GT3'} url={"https://upload.wikimedia.org/wikipedia/commons/c/c1/Porsche_992_GT3_1X7A0323.jpg"} />
+      <Main name={'Lamborghini Huracán STO'} url={"https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_detail/huracan/sto/2023/10_18_refresh/gallery/hura_sto_03.jpg"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap:30,
   },
 });
---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+
+export const Main = ({name, url}) => {
+  return (
+    <View style={styles.card}>
+        <Image
+          style={styles.img}
+          source={{uri:url}}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{name}</Text>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    img:{
+      width: 320,
+      height:180,
+      overflow:'hidden',
+      borderRadius:15,
+    },
+    
+    card:{
+      flex:0,
+      width: '90%',
+      height:300,
+      backgroundColor: '#F5F5F5',
+      alignItems:'center',
+      justifyContent: 'flex-end',
+      borderRadius:15,
+      shadowColor: "#000",
+      elevation: 20,
+      gap:5,
+
+    },
+
+    textContainer:{
+      flex:0,
+      width: '90%',
+      height:100,
+      alignItems:'flex-start',
+      justifyContent: 'flex-start',
+    },
+    title:{
+      fontSize: 20
+    },
+  });
+  
